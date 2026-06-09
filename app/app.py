3c7e4847c6908751702f4f5306aa0bf65ce736aa
abhr-0 <121384410+abhr-0@users.noreply.github.com>
2026-06-09 19:39:38 +0530

    Cache posters in app
    - Reduces load times for already fetched posters
    - Additionally, reduce srequests TMDB's API

--- app/app.py
+++ app/app.py
@@ -6,11 +6,12 @@ from dotenv import load_dotenv
 import os
 from math import exp
 
+@st.cache_data(show_spinner="Fetching results...")
 def fetch_poster(movie_id):
     response = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}")
     data = response.json()
     
-    return "https://images.tmdb.org/t/p/w500" + data["poster_path"]
+    return requests.get("https://images.tmdb.org/t/p/w500" + data["poster_path"]).content
 
 def recommend(movie):
     movie_index = movies[movies["title"] == movie].index[0]
@@ -18,17 +19,16 @@ def recommend(movie):
 
     movies_list = sorted(list(enumerate(distances)), reverse=True, key= lambda x: x[1])[1:6]
     
-    recommended_movies = []
-    recommended_posters = []
+    recommended_titles = []
+    recommended_movie_ids = []
     similarity_scores = []
 
     for i in movies_list:
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movies.append(movies.iloc[i[0]].title)
-        recommended_posters.append(fetch_poster(movie_id))
+        recommended_titles.append(movies.iloc[i[0]].title)
+        recommended_movie_ids.append(movies.iloc[i[0]].movie_id)
         similarity_scores.append(i[1])
     
-    return recommended_movies, recommended_posters, similarity_scores
+    return recommended_titles, recommended_movie_ids, similarity_scores
 
 def scale_scores(scores):
     return [int(100 / (1 + exp(-12*(score - 0.20)))) for score in scores]
@@ -46,13 +46,13 @@ st.title("Movie Recommender System")
 selected = st.selectbox("Enter movie name:", movies["title"].values)
 
 if st.button("Recommend"):
-    recommendations, posters, raw_scores = recommend(selected)
+    titles, ids, raw_scores = recommend(selected)
 
     scaled_scores = scale_scores(raw_scores)
 
     for i in range(5):
         col1, col2 = st.columns([1, 3])
-        col1.image(posters[i])
+        col1.image(fetch_poster(ids[i]))
         with col2.container(vertical_alignment="distribute"):
-            st.header(recommendations[i], anchor=False, divider="gray")
+            st.header(titles[i], anchor=False, divider="gray")
             st.progress(scaled_scores[i], text=f"Similarity: {int(scaled_scores[i])}%")
